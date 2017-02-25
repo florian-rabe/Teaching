ac1c57f7e1553b9ab29f11f0dd5304339d79a202
Florian Rabe <florian-rabe@users.noreply.github.com>
2017-02-25 01:50:28 +0100

    extended to programming language

--- SaDS_17/scala/formalsystems/src/language/Main.scala
+++ SaDS_17/scala/formalsystems/src/language/Main.scala
@@ -1,31 +1,41 @@
 package language
 
 /*
- * a simple application that executes a program in out language 
+ * a simple application that bundles and tests all components 
  */
 object Main {
   /**
-   * first command line argument: source file containing the program
-   * remaining command line: term to interpret
+   * main method (command line arguments are passed as 'args')
+   * 
+   * first argument: source file containing the program
+   * remaining arguments: the term to interpret relative to that program
    */
   def main(args: Array[String]) {
+    // read the program
     val file = scala.io.Source.fromFile(args(0))
     val progS = file.getLines.mkString("\n")
     file.close
     
-    // get the program and check it
+    // parse the program and check it and print it
     val prog = new Parser(progS).parseContext
     Checker.checkContext(Context(Nil), prog)
-    println("program is " + Printer.printContext(prog))
+    println("context:\n" + Printer.printContext(prog) + "\n")
     
-    // get the term to interpret (the 'main' function)
+    // get the term to interpret (the 'main' function), check it, infer its type, print both
     val termS = args.tail.mkString(" ")
     val term = new Parser(termS).parseTerm
-    val tp = Checker.inferTypeOfTerm(prog, term)
-    println("main call is " + Printer.printTerm(term) + " of type " + Printer.printType(tp))
-    
+    val tp = Checker.inferType(prog, term)
+    println("main call: '" + Printer.printTerm(term) + "' of type '" + Printer.printType(tp) + "'\n")
+
+    println("************************")
+    // interpret term against prog
+    // first run all code in the declarations
     val progI = Interpreter.interpretContext(Context(Nil), prog)
-    val result = Interpreter.interpretTerm(progI, term)
-    println("result is " + Printer.printTerm(result))
+    // then run the main call and print the result
+    val termI = Interpreter.interpretTerm(progI, term)
+    println("************************")
+    
+    println("\ncontext after interpretation: \n" + Printer.printContext(progI) + "\n")
+    println("result of main call: " + Printer.printTerm(termI))
   }
 }
