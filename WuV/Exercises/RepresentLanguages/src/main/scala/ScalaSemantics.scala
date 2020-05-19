4e6e0f8cdd8162c3c2de3483266ae7a828afac82
Florian Rabe <florian.rabe@gmail.com>
2020-05-19 13:33:40 +0200

    added more comments

--- WuV/Exercises/RepresentLanguages/src/main/scala/ScalaSemantics.scala
+++ WuV/Exercises/RepresentLanguages/src/main/scala/ScalaSemantics.scala
@@ -1,5 +1,11 @@
 package wuv.languages.example
 
+/** a semantics that interprets the syntax directly as Scala objects
+  * Thus, Scala itself is the target language.
+  */
+
+/** the basic operations in Scala */
+/** methods in an object are globally available via qualified names, corresponds to static methods in Java */
 object ScalaOperations {
   def conc(x:String, y: String): String = x+y
   def replace(x:String, c: Char, y:String): String = {
@@ -9,16 +15,26 @@ object ScalaOperations {
   def startsWith(x:String, y:String): Boolean = y.startsWith(x)
 }
 
+/** make operations available without qualification */
 import ScalaOperations._
 
+/**
+  * Specifically, our Scala semanticsinterprets
+  *   words of non-terminal N as Scala strings
+  *   words of non-terminal F as Scala Booleans
+  */
 object ScalaSemantics {
+  /* one translation function per non-terminal */
   def translate_N(x: N): String = {
     x match {
+      /* one case per production */
       case Zero() => ""
       case One() => "|"
       case Sum(m,n) =>
+        /* one recursive call per constructor argument */
         val mT = translate_N(m)
         val nT = translate_N(n)
+        /* compositionally building the result for the case */
         conc(mT,nT)
       case Product(m,n) =>
         val mT = translate_N(m)
--- WuV/Exercises/RepresentLanguages/src/main/scala/Syntax.scala
+++ WuV/Exercises/RepresentLanguages/src/main/scala/Syntax.scala
@@ -1,13 +1,21 @@
 package wuv.languages.example
 
+/* one type (= abstract class) per non-terminal */
+/* 'sealed' tells Scala that no other subclasses exist */
 sealed abstract class N
 
+/* one constructor (= abstract class) per production */
+/* we need to invent names for the productions, (here 'Zero') */
 case class Zero() extends N {
+  /* concrete syntax only appears via a parser (omitted here) and serializer ('toString') */
+  /* Scala auto-generates toString methods, so we need to 'override' */
   override def toString = "0"
 }
 case class One() extends N {
   override def toString = "1"
 }
+/* one constructor argument per occurrence of a non-terminal on the right-hand side of the production */
+/* we need to invent names for the constructor arguments (here 'left', 'right') */
 case class Sum(left: N, right: N) extends N {
   override def toString = left.toString + "+" + right.toString
 }
