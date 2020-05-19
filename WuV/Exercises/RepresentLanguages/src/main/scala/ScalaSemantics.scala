e404d713de50bbfe33c2d9d40152a437523b2885
Florian Rabe <florian.rabe@gmail.com>
2020-05-19 12:52:16 +0200

    semantics example

---
+++ WuV/Exercises/RepresentLanguages/src/main/scala/ScalaSemantics.scala
@@ -0,0 +1,42 @@
+package wuv.languages.example
+
+object Operations {
+  def conc(x:String, y: String): String = x+y
+  def replace(x:String, c: Char, y:String): String = {
+    x.transform(i => if (i == c) y else i)
+  }
+  def equal(x:String, y:String): Boolean = x == y
+  def startsWith(x:String, y:String): Boolean = x.startsWith(y)
+}
+
+import Operations._
+
+object ScalaSemantics {
+  def translate_N(x: N): String = {
+    x match {
+      case Zero() => ""
+      case One() => "|"
+      case Sum(m,n) =>
+        val mT = translate_N(m)
+        val nT = translate_N(n)
+        conc(mT,nT)
+      case Product(m,n) =>
+        val mT = translate_N(m)
+        val nT = translate_N(n)
+        replace(mT, '|', nT)
+    }
+  }
+
+  def translate_F(x: F): Boolean = {
+    x match {
+      case Equality(m,n) =>
+        val mT = translate_N(m)
+        val nT = translate_N(n)
+        equal(mT,nT)
+      case Ordering(m,n) =>
+        val mT = translate_N(m)
+        val nT = translate_N(n)
+        startsWith(mT,nT)
+    }
+  }
+}
---
+++ WuV/Exercises/RepresentLanguages/src/main/scala/Syntax.scala
@@ -0,0 +1,36 @@
+package wuv.languages.example
+
+sealed abstract class N
+
+case class Zero() extends N {
+  override def toString = "0"
+}
+case class One() extends N {
+  override def toString = "1"
+}
+case class Sum(left: N, right: N) extends N {
+  override def toString = left.toString + "+" + right.toString
+}
+case class Product(left: N, right: N) extends N {
+  override def toString = left.toString + "*" + right.toString
+}
+
+sealed abstract class F
+
+case class Equality(left: N, right: N) extends F {
+  override def toString = left.toString + "=" + right.toString
+}
+case class Ordering(left: N, right: N) extends F {
+  override def toString = left.toString + "<=" + right.toString
+}
+
+
+
+
+
+object Test {
+  def main(args: Array[String]) {
+    println("hello")
+  }
+}
+
