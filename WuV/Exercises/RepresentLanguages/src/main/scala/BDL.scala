eca7383bd8971fb6080a420a3e514c0873eea443
Florian Rabe <florian.rabe@gmail.com>
2020-06-23 13:39:45 +0200

    no message

--- WuV/Exercises/RepresentLanguages/src/main/scala/BDL.scala
+++ WuV/Exercises/RepresentLanguages/src/main/scala/BDL.scala
@@ -4,10 +4,11 @@ package wuv.exercise4
 object BDL {
   /** types */
   abstract class Type
-  case object ItgType extends Type
-  case object FltType extends Type
-  case object BolType extends Type
-  case object StrType extends Type
+  abstract class BaseType extends Type
+  case object ItgType extends BaseType
+  case object FltType extends BaseType
+  case object BolType extends BaseType
+  case object StrType extends BaseType
   case class LstType(elemType: Type) extends Type
   case class RecType(fields: List[(String,Type)]) extends Type
 
@@ -43,6 +44,64 @@ object BDL {
   }
 }
 
+import BDL._
+
+case class IllFormedCode(code: String, expectedType: Type) extends Throwable
+
+object AbstractCodecs {
+  type Code = String
+
+  abstract class Codec(val tp: Type) {
+    def encode(data: Data): Code
+
+    /** partial: may thrown IllFormedCode */
+    def decode(code: Code): Data
+  }
+
+  abstract class LstCodecOperator {
+    def makeCodec(elemCodec: Codec): Codec
+  }
+
+  abstract class RecCodecOperator {
+    def makeCodec(fieldCodecs: List[Codec]): Codec
+  }
+}
+
+import AbstractCodecs._
+
+/** boilerplate code for storing available codecs, choosing a codec, and calling encode/decode  */
 object Coding {
-  
+  /** error thrown during encoding */
+  case class NoCodecFound(tp: Type) extends Throwable
+
+  /* register the known codecs here */
+  val baseCodecs : List[Codec] = Nil
+  val lstCodecOp : Option[LstCodecOperator] = None
+  val recCodecOp : Option[RecCodecOperator] = None
+
+  def chooseCodec(tp: Type): Codec = tp match {
+    case bt: BaseType => baseCodecs.find(c => c.tp == tp) match {
+      case Some(c) => c
+      case None => throw NoCodecFound(tp)
+    }
+    case LstType(t) =>
+      val elemCodec = chooseCodec(t)
+      val op = lstCodecOp.getOrElse(throw NoCodecFound(tp))
+      op.makeCodec(elemCodec)
+    case RecType(fs) =>
+      val fieldCodecs = fs.map {case (k,t) => chooseCodec(t)}
+      val op = recCodecOp.getOrElse(throw NoCodecFound(tp))
+      op.makeCodec(fieldCodecs)
+  }
+
+  def encode(data: Data): Code = {
+    val tp = inferType(data)
+    val codec = chooseCodec(tp)
+    codec.encode(data)
+  }
+
+  def decode(code: Code, tp: Type): Data = {
+    val codec = chooseCodec(tp)
+    codec.decode(code)
+  }
 }
